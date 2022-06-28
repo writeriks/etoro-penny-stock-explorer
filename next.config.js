a62853c0c0988c673428d6b9173786394a59da09
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2022-06-28 18:58:51 +0200

    add api key to next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_FPM_API_KEY: '8c702d881d4e0f46482e04e1513f3fe4',
+  },
   reactStrictMode: true,
   images: {
     loader: 'akamai',
